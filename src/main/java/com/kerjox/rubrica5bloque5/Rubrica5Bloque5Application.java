a8ac12680e0307ac4e6b2594eff83e4edea48fe3
kerjox <kerjoxgamer095@gmail.com>
2022-02-15 08:10:44 +0100

    First commit

---
+++ src/main/java/com/kerjox/rubrica5bloque5/Rubrica5Bloque5Application.java
@@ -0,0 +1,13 @@
+package com.kerjox.rubrica5bloque5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rubrica5Bloque5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rubrica5Bloque5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerjox/rubrica5bloque5/Rubrica5Bloque5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerjox.rubrica5bloque5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rubrica5Bloque5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
