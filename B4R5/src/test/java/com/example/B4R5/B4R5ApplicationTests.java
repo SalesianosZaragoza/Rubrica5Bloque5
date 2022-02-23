218107144f07cb8b1a7f14373be8195610360466
AlexLacasta13 <78559705+AlexLacasta13@users.noreply.github.com>
2022-02-23 11:52:25 +0100

    Add files via upload

---
+++ B4R5/src/test/java/com/example/B4R5/B4R5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B4R5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B4R5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
